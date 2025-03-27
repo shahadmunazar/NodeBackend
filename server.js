7b33de6b4c66f2d059760da71bd99656900260d9
Mahadev <devraj40u@gmail.com>
2025-03-27 16:09:30 +0530

    Your commit message

--- server.js
+++ server.js
@@ -1,13 +1,24 @@
 const express = require("express");
-const userRoutes = require("./routes/userRoutes"); // Import the routes
+const cors = require("cors"); // Import cors
+const userRoutes = require("./routes/userRoutes");
 const LoginRoutes = require("./routes/authRoutes");
 
 const app = express();
+
+app.use(cors({
+    origin: "*",  // Allows all origins
+    methods: "GET, POST, PUT, DELETE, OPTIONS", // Allows all common methods
+    allowedHeaders: "*",  // Allows all headers
+}));
+
+// Enable CORS for all origins
+app.use(cors());
+
+// Middleware to parse JSON
 app.use(express.json());
 
-
 app.use("/api/auth", userRoutes);
-app.use('/api',LoginRoutes);
+app.use("/api", LoginRoutes);
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
