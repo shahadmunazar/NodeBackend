0869d0c888b9add70718b83ea38f30fcd13caf65
avd2021 <avdeshy213@gmail.com>
2025-03-28 11:22:40 +0530

    new

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const userRoutes = require("./routes/userRoutes"); // Import the routes
 const LoginRoutes = require("./routes/authRoutes");
+const cors = require("cors"); // Import cors
 
 const app = express();
+app.use(cors({
+    origin: "*", 
+    methods: "GET, POST, PUT, DELETE, OPTIONS", // Allows all common methods
+    allowedHeaders: "*",
+}));
+
+
 app.use(express.json());
 
+
+app.use(cors());
 
 app.use("/api/auth", userRoutes);
 app.use('/api',LoginRoutes);
