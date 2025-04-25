6d1da1c6fb25049cb0743a07f23b293909d85cc1
Mahadev <devraj40u@gmail.com>
2025-04-25 12:58:50 +0530

    Added Again

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const { login, getCurrentUser, verifyOtp } = require("../controllers/authController");
-const { ForgetPassword, UpdatePassword,GetLocation } = require("../controllers/API/SuperAdminController/ProfileController");
+const { ForgetPassword, UpdatePassword,GetLocation,SendEmailAgain } = require("../controllers/API/SuperAdminController/ProfileController");
 const { SubmitEnquiry } = require("../controllers/API/EnquirySection/enquiryController");
 const { authenticateUser } = require("../middleware/auth");
 
@@ -9,6 +9,7 @@ const router = express.Router();
 
 router.get('/get-location', GetLocation)
 router.post("/login", login);
+router.post("/send-email-to-orginazation-accept-inviation", SendEmailAgain);
 router.post("/submit-enquiry", SubmitEnquiry);
 router.post("/verify-otp", verifyOtp); // Step 2: Verify OTP & generate token
 router.post("/forget-password", ForgetPassword);
