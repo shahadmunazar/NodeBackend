25b32685856c0b976fa5cbdeb11bfdf83ebcc45a
avd2021 <avdeshy213@gmail.com>
2025-04-28 11:36:04 +0530

    newest11

--- seeders/20250304075930-demo-user-roles.js
+++ seeders/20250304075930-demo-user-roles.js
@@ -10,7 +10,7 @@ module.exports = {
 
     return queryInterface.bulkInsert("UserRoles", [
       {userId: userMap['shahadmunazar@gmail.com'],roleId: roleMap['superadmin'],createdAt: new Date(), updatedAt: new Date()},
-      {userId: userMap['avdeshy213@gmail.com'],roleId: roleMap['superadmin'],createdAt: new Date(), updatedAt: new Date()},
+      {userId: userMap['avdeshy213@gmail.com'],roleId: roleMap['organization'],createdAt: new Date(), updatedAt: new Date()},
       { userId: userMap["shahad1932@gmail.com"], roleId: roleMap["contractor"], createdAt: new Date(), updatedAt: new Date() },
       { userId: userMap["shahad1932a@gmail.com"], roleId: roleMap["admin"], createdAt: new Date(), updatedAt: new Date() },
       { userId: userMap["manager@example.com"], roleId: roleMap["manager"], createdAt: new Date(), updatedAt: new Date() },
